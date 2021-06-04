4776bcb8e762fce4c3268dc3a881a52ae1f9ef4a
Amr Zayet <amrz.edu@gmail.com>
2021-06-05 00:48:42 +0300

    1)Modified the cmputerFeatureID class: added @Data, constructors annotations to allow the creation of it to be used for the ComputerFeatureService class. 2)Added the ComputerFeatureService class which contains multiple functions to add computerFeatures using the compossite key, deleting the computerFeature and getting specific or all computerFeatures. 3)Added the ComputerFeatureController class for the http requests' handling

--- src/main/java/com/example/demo/entity/ComputerFeatureID.java
+++ src/main/java/com/example/demo/entity/ComputerFeatureID.java
@@ -1,8 +1,15 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Embeddable
 public class ComputerFeatureID implements Serializable {
     private int computerID;
