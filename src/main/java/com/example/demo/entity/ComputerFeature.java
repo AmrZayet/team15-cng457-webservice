3d4419473bf5b252f0e0da86ef18ad01c45cf3c1
Amr Zayet <amrz.edu@gmail.com>
2021-06-04 23:30:49 +0300

    Added @JsonIdentityInfo for Computer, Feature and ComputerFeture classes

--- src/main/java/com/example/demo/entity/ComputerFeature.java
+++ src/main/java/com/example/demo/entity/ComputerFeature.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,7 @@ import javax.persistence.ManyToOne;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class ComputerFeature {
 
     @EmbeddedId
--- src/main/java/com/example/demo/entity/Feature.java
+++ src/main/java/com/example/demo/entity/Feature.java
@@ -1,5 +1,7 @@
 package com.example.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "featureID")
 @Table(name="Feature")
 public class Feature {
 
